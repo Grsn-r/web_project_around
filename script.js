672f87ee863b3963398fd79a723b0ed087c57e17
grsn_ <gers.e02@gmail.com>
2025-08-28 17:46:45 -0600

    toques finales

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ const editButton = profile.querySelector('.edit-button')
 const form = document.querySelector('.form')
 const closeButton = form.querySelector('.form__close-icon')
 const fade = document.querySelector('.fade')
+const profileInfo = profile.querySelector('.profile__info')
+const saveButton = form.querySelector('.save-button')
+const elements = document.querySelector('.elements')
+
+let profileName = form.querySelector('#name');
+let about = form.querySelector('#about');
+
+let nameElement = profileInfo.querySelector('.profile__info_name');
+let aboutElement = profileInfo.querySelector('.profile__info_explorer');
 
 function showForm(){
     form.setAttribute("style", "display: flex");
     fade.setAttribute("style", "display: block");
+    saveButton.classList.add('save-button--inactive');
 }
 
 function hideForm(){
@@ -16,15 +26,6 @@ function hideForm(){
 
 editButton.addEventListener('click', showForm);
 closeButton.addEventListener('click', hideForm);
-
-const profileInfo = profile.querySelector('.profile__info')
-const saveButton = form.querySelector('.save-button')
-
-let profileName = form.querySelector('#name');
-let about = form.querySelector('#about');
-
-let nameElement = profileInfo.querySelector('.profile__info_name');
-let aboutElement = profileInfo.querySelector('.profile__info_explorer');
 
 function addProfileInfo(evt){
 evt.preventDefault();
@@ -36,13 +37,21 @@ saveButton.addEventListener('click',addProfileInfo);
 
 function validateContent(){
 if ((profileName.value === "") || (about.value === "")) {
-    saveButton.classList.add('save-button--disabled');
     saveButton.disabled = true;
+    saveButton.classList.add('save-button--inactive');
 } else {
-    saveButton.classList.remove('save-button--disabled');
     saveButton.disabled = false;
+    saveButton.classList.remove('save-button--inactive');
 }
 }
 
 profileName.addEventListener('input', validateContent);
 about.addEventListener('input', validateContent);
+
+const likeButton = elements.querySelector('.block__button')
+
+function likedPicture(){
+    likeButton.classList.toggle('block__button-black');
+}
+
+likeButton.addEventListener('click', likedPicture);
