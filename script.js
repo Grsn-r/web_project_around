dffc08689f63de2045e1bda02ca1415db00af78b
grsn_ <gers.e02@gmail.com>
2025-08-28 13:48:25 -0600

    la info de perfil ahora es editable

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let profile = document.querySelector('.profile')
-let editButton = profile.querySelector('.edit-button')
-let form = document.querySelector('.form')
-let closeButton = form.querySelector('.form__close-icon')
-let fade = document.querySelector('.fade')
+const profile = document.querySelector('.profile')
+const editButton = profile.querySelector('.edit-button')
+const form = document.querySelector('.form')
+const closeButton = form.querySelector('.form__close-icon')
+const fade = document.querySelector('.fade')
 
 function showForm(){
     form.setAttribute("style", "display: flex");
@@ -11,8 +11,25 @@ function showForm(){
 
 function hideForm(){
     form.setAttribute("style", "display: none");
-    fade.setAttribute("style", "display: none")
+    fade.setAttribute("style", "display: none");
 }
 
 editButton.addEventListener('click', showForm);
 closeButton.addEventListener('click', hideForm);
+
+const profileInfo = profile.querySelector('.profile__info')
+const saveButton = form.querySelector('.save-button')
+
+function addProfileInfo(evt){
+evt.preventDefault();
+let name = form.querySelector('.name');
+let about = form.querySelector('.about');
+
+let nameElement = profileInfo.querySelector('.profile__info_name');
+let aboutElement = profileInfo.querySelector('.profile__info_explorer');
+
+nameElement.textContent = name.value;
+aboutElement.textContent = about.value;
+hideForm();   
+}
+saveButton.addEventListener('click',addProfileInfo);
